98dc1f4f428d2413e2a7c063a38b7e3bc38f38ca
Михаил Дорошин <demargorn@gmail.com>
2024-09-21 23:23:59 +0300

    rollback changes

--- src/app.js
+++ src/app.js
@@ -1,23 +1,12 @@
+import { read } from './readFn';
+import { json } from './jsonFn';
+
 (async () => {
-   return new Promise((resolve) => {
-      setTimeout(() => {
-         const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
-         return ((input) => {
-            const buffer = new ArrayBuffer(input.length * 2);
-            const bufferView = new Uint16Array(buffer);
-            for (let i = 0; i < input.length; i++) {
-               bufferView[i] = input.charCodeAt(i);
-            }
-            resolve(buffer);
-         })(data);
-      }, 1000);
-   })
-      .then((data) => {
-         return new Promise((resolve) => {
-            setTimeout(() => {
-               resolve(String.fromCharCode.apply(null, new Uint16Array(data)));
-            }, 500);
-         });
-      })
-      .catch((err) => console.log(err));
+   try {
+      const data = await read();
+      const value = await json(data);
+      return value;
+   } catch (err) {
+      console.log(err);
+   }
 })();
